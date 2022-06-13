352e8b43c8981fcc26ad47821b61e02ca40636d0
tagei6422 <735765574@qq.com>
2022-06-13 14:40:22 +0800

    week09

--- week09/130. 被围绕的区域.java
+++ week09/130. 被围绕的区域.java
@@ -8,28 +8,33 @@
 // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 class Solution {
-    int[] fa;
     int m;
     int n;
+    int[] fa;
     public void solve(char[][] board) {
-        int[] dx = {1, 0, 0, -1};
-        int[] dy = {0, 1, -1, 0};
         m = board.length;
         n = board[0].length;
-        fa = new int[m * n + 1];
         int outside = m * n;
+        fa = new int[m * n + 1];
         for(int i = 0; i <= m * n; i++) fa[i] = i;
+        // 方向数组，只需要两个方向，向右、向下
+        int[] dx = {1, 0};
+        int[] dy = {0, 1};
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(board[i][j] == 'X') continue;
-                for(int k = 0; k < 4; k++){
+                if(board[i][j] == 'X') continue; // 跳过X
+                // 对O进行如下遍历
+                int curr = getNum(i, j);
+                for(int k = 0; k < 2; k++){
                     int ni = i + dx[k];
                     int nj = j + dy[k];
-                    if(ni < 0 || nj < 0 || ni >= m || nj >= n){
-                        unionSet(num(i, j), outside);
+                    // 超过范围或在边缘上，证明该O点在边缘，让它和outside并入同一个集合
+                    if(ni <= 0 || nj <= 0 || ni >= m || nj >= n){
+                        unionSet(curr, outside);
                     }else{
+                        // 如果下一步在范围内，让它和相邻的O点并入同一个集合
                         if(board[ni][nj] == 'O'){
-                            unionSet(num(i, j), num(ni, nj));
+                            unionSet(curr, getNum(ni, nj));
                         }
                     }
                 }
@@ -38,13 +43,11 @@ class Solution {
         outside = find(outside);
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(board[i][j] == 'O' && find(num(i, j)) != outside){
-                    board[i][j] = 'X';
-                }
+                if(board[i][j] == 'O' && find(getNum(i, j)) != outside) board[i][j] = 'X';
             }
         }
     }
-    private int num(int i, int j){
+    private int getNum(int i, int j){
         return i * n + j;
     }
     private int find(int i){
